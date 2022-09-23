6148dc1d3f3d04bd010c777030f0a31317e72030
Andrew Snodgrass <git@pointlander.info>
2022-09-22 21:40:43 -0500

    Added fft2

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"image/png"
 	"log"
 	"os"
+
+	"github.com/mjibson/go-dsp/fft"
 )
 
 func main() {
@@ -26,13 +28,20 @@ func main() {
 
 	b := img.Bounds()
 	set := image.NewRGBA(b)
+	xx := make([][]complex128, b.Max.Y)
 	for y := 0; y < b.Max.Y; y++ {
+		xx[y] = make([]complex128, b.Max.X)
 		for x := 0; x < b.Max.X; x++ {
 			original := img.At(x, y)
 			pixel := color.GrayModel.Convert(original)
+			gray, _ := pixel.(color.Gray)
+			xx[y][x] = complex(float64(gray.Y), 0)
 			set.Set(x, y, pixel)
 		}
 	}
+
+	y := fft.FFT2(xx)
+	_ = y
 
 	output, err := os.Create("gray.jpg")
 	if err != nil {
